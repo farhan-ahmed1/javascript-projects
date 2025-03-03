e74a8072fbd9e117f985c76ffcfd2bbd39f3d5f6
Farhan Ahmed <fahmed@iastate.edu>
2025-03-02 23:09:07 -0600

    working delete functionality

--- course-2/todo-app/script.js
+++ course-2/todo-app/script.js
@@ -49,7 +49,11 @@ const updateTaskContainer = () => {
 };
 
 const deleteTask = (buttonEl) => {
-  
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
 }
 
 const reset = () => {
